97834d6602764b1ba48947d69938f20ad49b6c21
harrytripp <115900915+harrytripp@users.noreply.github.com>
2025-01-02 18:54:30 +0000

    Created basic skeleton of functions

--- app/app.py
+++ app/app.py
@@ -1,9 +1,28 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+from markupsafe import escape
 
-@app.route('/')
+# example using escape to protect against injection attacks when returning html
+#@app.route("/<name>")
+#def hello(name):
+#    return f"Hello, {escape(name)}!"
+#app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
 	return "Hello World!"
+
+def todoList():
+    if request.method == 'POST':
+        return addTask()
+    else:
+        return showTasks()
+
+def addTask():
+		return "Task added!"
+
+def showTasks():
+		return "List of tasks"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
