94490a218fa1ad2b7d96e52fd3cdc25ee319e722
harrytripp <115900915+harrytripp@users.noreply.github.com>
2025-01-05 15:02:22 +0000

    Created database. Getting 404 error when running

--- app/app.py
+++ app/app.py
@@ -1,16 +1,32 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, g
 from markupsafe import escape
+import sqlite3
 
 # example using escape to protect against injection attacks when returning html
 #@app.route("/<name>")
 #def hello(name):
 #    return f"Hello, {escape(name)}!"
-#app = Flask(__name__)
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
+       
+DATABASE = 'app/database.db'
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
-	return "Hello World!"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 def todoList():
     if request.method == 'POST':
@@ -24,5 +40,6 @@ def addTask():
 def showTasks():
 		return "List of tasks"
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+def createTable():
+    get_db()
+    db.execute("CREATE TABLE IF NOT EXISTS example (id INTEGER, name TEXT, info TEXT)")
